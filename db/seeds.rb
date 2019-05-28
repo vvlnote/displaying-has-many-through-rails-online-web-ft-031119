029d4ab7ba8b4dc8015e18e1b79c92f573500eca
Joy Lin <vvlnote@hotmail.com>
2019-05-28 18:38:02 +0000

    Automatically backed up by Learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+resources :posts, only: [:show]
+resources :users, only: [:show]
